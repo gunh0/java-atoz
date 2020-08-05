115da1ff443d3b17eb518dc943c72be3ad980b97
gunh0 <devgun@kakao.com>
2020-08-05 20:17:19 +0900

    add: jdk 11 docker & regex example

---
+++ this-is/README.md
@@ -0,0 +1,7 @@
+### Python virtual Env.
+
+```
+virtualenv venv
+source ./venv/source/bin/activate
+python3 -m pip install -r requirements.txt
+```
--- this-is/demo-002/src/main/java/org/example/Main.java
+++ this-is/demo-002/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Notice, do not import com.mysql.cj.jdbc.*
+// or you will have problems!
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
     }
 }
