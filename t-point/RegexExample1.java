9a8d87a4743a4ac0d19b3afd5cc789055005f53a
gunh0 <devgun@kakao.com>
2020-08-08 23:11:34 +0900

    add: reg. example

--- t-point/RegexExample1.java
+++ t-point/RegexExample1.java
@@ -3,7 +3,10 @@ import java.util.regex.*;
 public class RegexExample1 {
     public static void main(String args[]) {
         // 1st way
-        Pattern p = Pattern.compile(".s");// . represents single character
+
+        Pattern p = Pattern.compile(".s");
+        // . represents single character
+
         Matcher m = p.matcher("as");
         boolean b = m.matches();
 
