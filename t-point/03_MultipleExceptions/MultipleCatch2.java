cc060ceb4ecae9ccc37afb40eb048662df2feec3
Gunho Park <devgun@kakao.com>
2020-10-07 20:32:31 +0900

    add: multiple exceptions

--- t-point/03_MultipleExceptions/MultipleCatch2.java
+++ t-point/03_MultipleExceptions/MultipleCatch2.java
@@ -1,9 +1,10 @@
-public class MultipleCatch {
+public class MultipleCatch2 {
     public static void main(String[] args) {
 
         try {
             int a[] = new int[5];
-            a[5] = 30 / 0;
+
+            System.out.println(a[10]);
         } catch (ArithmeticException e) {
             System.out.println("Arithmetic Exception occurs");
         } catch (ArrayIndexOutOfBoundsException e) {
