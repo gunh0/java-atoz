549e8e37d5fd188e55da72828476043f3d995a82
gunh0 <devgun@kakao.com>
2020-10-13 23:40:22 +0900

    add: multiple exceptions

---
+++ t-point/03_MultipleExceptions/MultipleCatch5.java
@@ -0,0 +1,17 @@
+public class MultipleCatch5 {
+    public static void main(String args[]) {
+        try {
+            int a[] = new int[5];
+            a[5] = 30 / 0;
+            
+        // error: exception ArithmeticException has already been caught
+        // } catch (Exception e) {
+        //     System.out.println("common task completed");
+        } catch (ArithmeticException e) {
+            System.out.println("task1 is completed");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("task 2 completed");
+        }
+        System.out.println("rest of the code...");
+    }
+}
