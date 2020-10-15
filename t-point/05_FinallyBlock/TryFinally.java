da52e294d23ec8956307e6db7b1095d32fee41d2
Gunho Park <devgun@kakao.com>
2020-10-16 03:03:53 +0900

    add: finally block

---
+++ t-point/05_FinallyBlock/TryFinally.java
@@ -0,0 +1,18 @@
+public class TryFinally {
+    public static void main(String args[]) {
+        try {
+            //below code do not throw any exception
+            int data = 25 / 5;
+            System.out.println(data);
+        }
+        //catch won't be executed
+        catch (NullPointerException e) {
+            System.out.println(e);
+        }
+        //executed regardless of exception occurred or not
+        finally {
+            System.out.println("finally block is always executed");
+        }
+        System.out.println("rest of phe code...");
+    }
+}
